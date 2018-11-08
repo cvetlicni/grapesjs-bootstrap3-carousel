5bfb83aed522016f650898005fdb7a1a0e52caaa
Omar <omarmd1986@outlook.com>
2018-11-08 11:46:49 -0800

    Creating the block

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import loadComponents from './components';
 import loadBlocks from './blocks';
 
-export default (editor, opts = {}) => {
-  const options = { ...{
-    // default options
-  },  ...opts };
+export default grapesjs.plugins.add('grapesjs-simple-lp', (editor, opts = {}) => {
 
-  // Add components
-  loadComponents(editor, options);
+    const options = {...{
+                prefixName: 'bst-carousel',
+                
+                gridsCategory: 'Extra'
+        }, ...opts};
 
-  // Add blocks
-  loadBlocks(editor, options);
+    // Add components
+    loadComponents(editor, options);
 
-  // TODO Remove
-  editor.on('load', () => editor.addComponents(`<div style="margin:100px; padding:25px;">Content loaded from the plugin</div>`, { at: 0 }))
-};
+    // Add blocks
+    loadBlocks(editor, options);
+});
